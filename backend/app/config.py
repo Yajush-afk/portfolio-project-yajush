cacaac25fb6e1cb17a4b6d05d7176652bcab181b
Yajush-afk <yajush24606@gmail.com>
2026-01-07 16:12:35 +0530

    "Backend ko nikalo game se"

--- backend/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    ADMIN_SECRET_KEY = os.getenv("ADMIN_SECRET_KEY")
-    PROJECT_NAME = "Portfolio API"
-    VERSION = "1.0.0"
